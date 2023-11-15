9b178ccea115d57732d85791c3751c95817a065e
Wendz19 <149917375+Wendz19@users.noreply.github.com>
2023-11-15 17:44:18 +0200

    Updated the JavaScript file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 function search(event) {
   event.preventDefault();
-  console.log(event);
 }
 
-let searchForm = document.querySelector("#search-form");
+function displayTemp(response) {
+  console.log(response.data);
+}
+let city = `response.data.city`;
+let apiKey = `43dft5a84ob9a003f0c493a68e6b1dbe`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayTemp);
+
+let searchForm = document.querySelector("#search-bar");
 searchForm.addEventListener("submit", search);
