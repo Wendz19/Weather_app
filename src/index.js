67773f01202abc2f462754224bca583dcc5e5ec2
Wendz19 <149917375+Wendz19@users.noreply.github.com>
2023-11-16 02:00:18 +0200

    Added several functions to JS file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-function search(event) {
+function getData(response) {
+  let temperature = response.data.temperature.current;
+  let currentTemp = document.querySelector("#temperature");
+  currentTemp.innerHTML = Math.round(temperature);
+}
+
+function searchTemperature(event) {
   event.preventDefault();
+  let headingElement = document.querySelector("#city");
+  let searchInput = document.querySelector("#search-bar");
+  headingElement.innerHTML = searchInput.value;
+  let cityInput = document.querySelector("#search-bar");
+  let city = cityInput.value;
+  let apiKey = `43dft5a84ob9a003f0c493a68e6b1dbe`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(getData);
 }
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchTemperature);
 
-function displayTemp(response) {
-  console.log(response.data);
-}
-let city = `response.data.city`;
-let apiKey = `43dft5a84ob9a003f0c493a68e6b1dbe`;
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key=${apiKey}`;
+//
+//
+//
+//
 
-axios.get(apiUrl).then(displayTemp);
-
-let searchForm = document.querySelector("#search-bar");
-searchForm.addEventListener("submit", search);
+//
+//
+//
+//
